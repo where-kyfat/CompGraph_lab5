ed2e3c32e9adac6f68a3e1e34479b6e3c6a099d5
Oleg Shabarshin <oleg_shabarshin@mail.ru>
2019-10-18 00:14:39 +0300

    L-system

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Graf5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(93, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 700);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 547);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 55);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 581);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 721);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Threading;
+using System.IO;
+
+struct rule
+{
+    public string a;
+    public string b;
+}
+
+namespace Graf5
+{
+    public partial class Form1 : Form
+    {
+        private rule[] rules = new rule[0];
+        private int strtangle = 270;
+        string axiom;
+        private float maxX, maxY, minX, minY, Ox, Oy;
+        private PointF curpoint;
+        private Tuple<PointF, PointF>[] pts = new Tuple<PointF, PointF>[0];
+        Graphics g;
+        private int strtlength = 100;
+        private int iter, angle, curangle;
+        Stack<Tuple<PointF, int>> rtrn = new Stack<Tuple<PointF, int>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+
+            Ox = 5;
+            Oy = pictureBox1.Height - 5;
+            minY = maxY = Oy;
+            minX = maxX = Ox;
+            curpoint = new PointF(Ox, Oy);
+
+            StreamReader sr = new StreamReader("rules.txt");
+            string fstrule = sr.ReadLine();
+            axiom = fstrule.Split(' ')[0];
+            angle = Int32.Parse(fstrule.Split(' ')[1]);
+            curangle = Int32.Parse(fstrule.Split(' ')[2]);
+            while (!sr.EndOfStream)
+            {
+                string rulerd = sr.ReadLine();
+                Array.Resize(ref rules, rules.Length + 1);
+                rules[rules.Length - 1].a = rulerd.Split('=')[0];
+                rules[rules.Length - 1].b = rulerd.Split('=')[1];
+            }
+            sr.Dispose();
+        }
+
+        private string lsystem()
+        {
+            string cur = axiom;
+            StringBuilder next = new StringBuilder();
+            for (int i = 0; i < iter; i++)
+            {
+                for (int j = 0; j < cur.Length; j++)
+                {
+                    bool flag = false;
+                    foreach (var r in rules)
+                    {
+                        if (cur[j].ToString() == r.a)
+                        {
+                            flag = true;
+                            next.Append(r.b);
+                            break;
+                        }
+                    }
+                    if (!flag)
+                        next.Append(cur[j]);
+                }
+                cur = next.ToString();
+                next.Clear();
+            }
+            return cur;
+        }
+
+        private void Forward(float distance)
+        {
+            var angleRadians = curangle * Math.PI / 180;
+            var newX = curpoint.X + (float)(distance * Math.Cos(angleRadians));
+            var newY = curpoint.Y + (float)(distance * Math.Sin(angleRadians));
+            maxX = newX > maxX ? newX : maxX;
+            maxY = newY > maxY ? newY : maxY;
+            minY = newY < minY ? newY : minY;
+            minX = newX < minX ? newX : minX;
+            Array.Resize(ref pts, pts.Length + 1);
+            pts[pts.Length - 1] = new Tuple<PointF, PointF>(curpoint, new PointF(newX, newY));
+            curpoint.X = newX;
+            curpoint.Y = newY;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            clear();
+            iter = Decimal.ToInt32(numericUpDown1.Value);
+            float length = strtlength / (float)Math.Pow(2, iter);
+            string pattern = lsystem();
+            foreach (var ch in pattern)
+            {
+                bool flag = false;
+                foreach (var r in rules)
+                {
+                    if (ch.ToString() == r.a)
+                    {
+                        flag = true;
+                        Forward(length);
+                        break;
+                    }
+                }
+                if (!flag)
+                    switch (ch)
+                    {
+                        case '[':
+                            rtrn.Push(new Tuple<PointF, int>(curpoint, curangle));
+                            break;
+                        case ']':
+                            var t = rtrn.Pop();
+                            curpoint = t.Item1;
+                            curangle = t.Item2;
+                            break;
+                        case '-':
+                            curangle -= angle;
+                            break;
+                        case '+':
+                            curangle += angle;
+                            break;
+                        default:
+                            break;
+                    }
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear()
+        {
+            g.Clear(Color.White);
+            minY = maxY = Oy;
+            minX = maxX = Ox;
+            curpoint.X = Ox;
+            curpoint.Y = Oy;
+            curangle = strtangle;
+            Array.Clear(pts, 0, pts.Length);
+            Array.Resize(ref pts, 0);
+            rtrn.Clear();
+            pictureBox1.Invalidate();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.TranslateTransform(pictureBox1.Width / 2 - (maxX + minX) / 2, (pictureBox1.Height) / 2 - (minY + maxY) / 2);
+            foreach (var i in pts)
+            {
+                e.Graphics.DrawLine(Pens.Black, i.Item1, i.Item2);
+            }
+        }
+    }
+}
